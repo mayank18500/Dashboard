66ff30cd50ed29ce76b58ebef56c97a32f63d53b
Mayank kumar <mayank01082005@gmail.com>
2025-12-10 15:04:31 +0530

    Fixes

--- backend/server.js
+++ backend/server.js
@@ -1,88 +1,91 @@
 import express from "express";
-import helmet, { contentSecurityPolicy } from "helmet";
+import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
-import { aj } from "./lib/arcjet.js";
 import path from "path";
 
 import productRoutes from "./routes/productRoutes.js";
 import { sql } from "./config/db.js";
+import { aj } from "./lib/arcjet.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const __dirname=path.resolve();
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cors());
-app.use(helmet({
-  contentSecurityPolicy:false,
-}
-));
-app.use(morgan("dev"));
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+); // helmet is a security middleware that helps you protect your app by setting various HTTP headers
+app.use(morgan("dev")); // log the requests
 
-//apply arcjet rate limits to all routes
+// apply arcjet rate-limit to all routes
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1, // specifies that each request consumes 1 token
+    });
 
-app.use(async(req,res,next)=>{
-  try{
-    const decision =await aj.protect(req,{
-      requested:1, //specifies that each request consumes 1 token
-    })
-
-    if(decision.isDenied()){
-      if(decision.reason.isRateLimit()){
-        res.status(429).json({error:"Too Many Requests",});
-      }else if(decision.reason.isBot()){
-        res.status(403).json({error:"Bot access denied"});
-      }else{
-        res.status(403).json({error:"Forbidden"});
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({ error: "Too Many Requests" });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ error: "Bot access denied" });
+      } else {
+        res.status(403).json({ error: "Forbidden" });
       }
-      return
-    }
-
-    //checck for spoof bots
-    if(decision.results.some((result)=>result.reason.isBot() && result.reason.isSpoofed())){
-      res.status(403).json({error:"Spoofed bot detected"});
       return;
     }
-    next()
-  }catch(error){
-    console.log("Arcjet error",error);
+
+    // check for spoofed bots
+    if (decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
+      res.status(403).json({ error: "Spoofed bot detected" });
+      return;
+    }
+
+    next();
+  } catch (error) {
+    console.log("Arcjet error", error);
     next(error);
   }
-})
+});
 
 app.use("/api/products", productRoutes);
 
-if(process.env.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname,"/frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+  // server our react app
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get(".*",(_req,res)=>{
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  })
+  });
 }
-
 
 async function initDB() {
   try {
     await sql`
-      CREATE TABLE IF NOT EXISTS products(
+      CREATE TABLE IF NOT EXISTS products (
         id SERIAL PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
         image VARCHAR(255) NOT NULL,
-        price DECIMAL(10,2) NOT NULL,
+        price DECIMAL(10, 2) NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
       )
     `;
-    console.log("Database Initialised Successfully");
+
+    console.log("Database initialized successfully");
   } catch (error) {
     console.log("Error initDB", error);
   }
 }
-initDB();
 
-app.listen(PORT, () => {
-  console.log("Server is running on port " + PORT);
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is running on port " + PORT);
+  });
 });
